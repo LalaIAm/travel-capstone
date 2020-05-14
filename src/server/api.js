19fda42661afd288748a849f3c396555b172f04c
Lauren Thorud <lthorud12@gmail.com>
2020-05-14 14:15:18 -0500

    setup initial server

---
+++ src/server/api.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const fetch = require('node-fetch');
+
+const {trips, stitch } = require('./db');
+
+const router = express.Router();
+
+
+
+
+
+module.exports = router;
---
+++ src/server/db.js
@@ -0,0 +1,24 @@
+const {
+    Stitch,
+    RemoteMongoClient,
+    AnonymousCredential
+} = require('mongodb-stitch-server-sdk');
+
+const appId = process.env.MONGODB_APPID;
+
+const stitch = Stitch.initializeDefaultAppClient(appId);
+
+stitch.auth.loginWithCredential(new AnonymousCredential())
+    .then(user => {
+        console.log('logged in anonymously as user: ', user.id)
+        
+    }).catch(err => console.log('db error: ', err));
+
+const mongo = stitch.getServiceClient(
+    RemoteMongoClient.factory,
+    'mongodb-atlas'
+)
+
+const trips = mongo.db('travelFar').collection('trips');
+
+module.exports = { trips, stitch };
